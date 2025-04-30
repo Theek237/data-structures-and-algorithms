8768fe327616e36d9747f2aa549731ac221fe45e
Theekshana Alahakoon <theek237@gmail.com>
2025-04-30 21:49:27 +0530

    Initial Commit

---
+++ Arrays/arrayOperations.js
@@ -0,0 +1,14 @@
+const stringArray = ["a", "b", "c", "d"];
+
+//push - add to the end
+stringArray.push("e"); //O(1)
+
+//pop - delete from end
+stringArray.pop(); //O(1)
+
+//unshift - add to the begining
+stringArray.unshift("z"); //O(n)
+
+console.log(stringArray);
+//splice - add to specific position
+stringArray.splice();
